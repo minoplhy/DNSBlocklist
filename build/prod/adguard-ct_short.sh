551d4299aac8897f8b1cac5fc0dda7f3fc8ffc4b
Minoplhy <kraneberru@disroot.org>
2021-04-13 02:55:21 +0000

    Update and rename adguard-ct.sh to adguard-ct_short.sh

---
+++ build/prod/adguard-ct_short.sh
@@ -0,0 +1,2 @@
+wget -O /root/adguard-ct/adguard_cname-tracker.txt https://github.com/AdguardTeam/cname-trackers/raw/master/combined_disguised_trackers.txt
+python /root/adguard-ct/adguard-host_rpz_sys.py /root/adguard-ct/adguard_cname-tracker.txt /root/adguard-ct/adguard_cname-tracker.rpz
