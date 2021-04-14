e9b70610a92807c987ac188fe72206310a1e9d9e
Minoplhy <1mapleonade@protonmail.com>
2021-04-14 04:26:45 +0000

    Adguard Host converter fixing

--- converter/template/adguard-host_rpz_argv.py
+++ converter/template/adguard-host_rpz_argv.py
@@ -27,15 +27,18 @@ for i in range(len(file)):
 #print(file)
 with open(infile, 'w') as f1:
     f1.writelines(["%s\n" % item  for item in file])
-f.close()	
+f.close()
 
 with open(infile, 'r') as f: # load file 
  lines = f.read().splitlines() # read lines
 with open(infile, 'w') as f: # load file in write mode
  for line in lines:
-  if not line.startswith(';'):
-   f.write('\n'.join([line + '  CNAME .\n'])) # add CNAME . if file does not start with ;
+  if line.startswith(';'):
+   f.write('\n'.join([line + '\n']))
+  elif not line.startswith(';'):
+   f.write('\n'.join([line + ' CNAME .\n'])) # add CNAME . if file does not start with ;   
 f.close()
+
 copyfile(infile, outfile) # copy input file to output file
 os.remove(infile) # remove input file
 
