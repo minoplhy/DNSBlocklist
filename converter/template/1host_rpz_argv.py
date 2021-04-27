9a4a089ef146ad1a8246e04d3c5ad1d126552115
Minoplhy <1mapleonade@protonmail.com>
2021-04-27 03:42:12 +0000

    Add files via upload

--- converter/template/1host_rpz_argv.py
+++ converter/template/1host_rpz_argv.py
@@ -6,24 +6,10 @@ from shutil import copyfile
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
-f = open(infile,'r')
-a = ['||','^','|']
-lst = []
-for line in f:
-    for word in a:
-        if word in line:
-            line = line.replace(word,'')
-    lst.append(line)
-f.close()
-f = open(infile,'w')
-for line in lst:
-    f.write(line)
-f.close()
-
 with open(infile) as f:
     file = f.read().split('\n')
 for i in range(len(file)):
-    file[i] = sub(r'!', ';', file[i])
+    file[i] = sub(r'#', ';', file[i])
 #print(file)
 with open(infile, 'w') as f1:
     f1.writelines(["%s\n" % item  for item in file])
@@ -35,11 +21,9 @@ with open(infile, 'w') as f: # load file in write mode
  for line in lines:
   if line.startswith(';'):
    f.write('\n'.join([line + '\n']))
-  elif line.startswith('@@'):
-   f.write('\n'.join([line + ' CNAME rpz-passthru.\n']))
   elif not line.strip():
    f.write('\n'.join([line + '\n']))
-  elif not line.startswith(';') and not line.startswith('@@') and line.strip():
+  elif not line.startswith(';') and line.strip():
    f.write('\n'.join([line + ' CNAME .\n'])) # add CNAME . if file does not start with ;   
 f.close()
 
