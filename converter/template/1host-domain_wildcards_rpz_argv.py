070b340b80898e07dacb993c9b9898f73f804af9
Minoplhy <1mapleonade@protonmail.com>
2021-04-27 04:49:50 +0000

    Add files via upload

--- converter/template/1host-domain_wildcards_rpz_argv.py
+++ converter/template/1host-domain_wildcards_rpz_argv.py
@@ -6,6 +6,20 @@ from shutil import copyfile
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
+f = open(infile,'r')
+a = ['*.']
+lst = []
+for line in f:
+    for word in a:
+        if word in line:
+            line = line.replace(word,'')
+    lst.append(line)
+f.close()
+f = open(infile,'w')
+for line in lst:
+    f.write(line)
+f.close()
+
 with open(infile) as f:
     file = f.read().split('\n')
 for i in range(len(file)):
