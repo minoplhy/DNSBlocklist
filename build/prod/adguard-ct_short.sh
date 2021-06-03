0355ecb023b12f783d884572774f527e02bf3b23
Minoplhy <zebr@m21.kylz.nl>
2021-06-03 07:27:02 +0000

    Remove building file because useless

--- build/prod/adguard-ct_short.sh
+++
@@ -1,2 +0,0 @@
-wget -O /root/adguard-ct/adguard_cname-tracker.txt https://github.com/AdguardTeam/cname-trackers/raw/master/combined_disguised_trackers.txt
-python3 /root/adguard-ct/adguard-host_rpz_sys.py /root/adguard-ct/adguard_cname-tracker.txt /root/adguard-ct/adguard_cname-tracker.rpz
